47ffb304a5a5e279ca91cd703689cdcbe97500af
Sulman-P <odhiambosulman3@gmail.com>
2026-07-20 12:56:17 +0300

    Create js folder with app.js

---
+++ js/app.js
@@ -0,0 +1,7 @@
+// Main application file
+console.log('Smart Class Allocation System - SCAS');
+
+// Application initialization
+document.addEventListener('DOMContentLoaded', function() {
+  console.log('Application loaded');
+});
